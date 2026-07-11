47ca9f89244fc65de6e9e9cc7a23169a33604320
GabrielCardoz0 <gabriel.scardozo7@gmail.com>
2026-07-10 21:35:49 -0300

    fix overlay

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 5555,
+    hmr: {
+      overlay: false
+    }
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
